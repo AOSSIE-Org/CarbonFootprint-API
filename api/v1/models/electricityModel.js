5bf91f0edbd67f29973bd52ee3e6f26ae9d0dd16
Saisankar Gochhayat <saisankargochhayat@gmail.com>
2017-06-01 16:26:53 +0530

    raw formatted data added

---
+++ api/v1/models/electricityModel.js
@@ -0,0 +1,24 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+// create a schema for emissions related to electricity
+var electricitySchema = new Schema({
+    country: String,
+    generation: {
+        co2: Number,
+        ch4: Number,
+        no2: Number
+    },
+    transmission: {
+        co2: Number,
+        ch4: Number,
+        no2: Number
+    },
+    consumption: {
+        co2: Number,
+        ch4: Number,
+        no2: Number
+    }
+}) 
+
+module.exports =  mongoose.model('Electricity', electricitySchema);
