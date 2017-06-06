25d7cba008e7c8c44e8fd9a20d2e50754c721267
Saisankar Gochhayat <saisankargochhayat@gmail.com>
2017-06-06 16:17:43 +0530

    post req test

--- api/v1/routes/emissionRoutes.js
+++ api/v1/routes/emissionRoutes.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var router = express.Router();
-
 // get the emission controller
 var Emission = require('../controllers/emissionController'); 
 
-router.get('/', Emission.find);
+router.post('/',function (req, res) {
+    // console.log(req.body);      // your JSON
+    // res.send(req.body);    // echo the result back
+    res.status(200).send(req.body);
+})
+// router.get('/', Emission.find);
 
 module.exports = router;
+//curl test- curl -H "Content-Type: application/json" -X POST -d '{"item":"electricity","region":"Africa","unit":"kg/kWh","quantity":1}' http://localhost:3080/v1/emissions
