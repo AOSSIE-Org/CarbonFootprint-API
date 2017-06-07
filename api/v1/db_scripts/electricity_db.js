1d7bcda18de449688e37b38758c649104455e57c
Saisankar Gochhayat <saisankargochhayat@gmail.com>
2017-06-07 20:40:52 +0530

    changed as per suggestions

--- api/v1/db_scripts/electricity_db.js
+++ api/v1/db_scripts/electricity_db.js
@@ -28,55 +28,29 @@ mongoose.connection.on('error', (err) => {
 mongoose.connection.on('disconnected', () => {  
   console.log('Database disconnected'); 
 });
-var Emission = require('../models/emissionschema.js')
+var Emission = require('../models/emissionModel.js')
 var json = require('../../../raw_data/electricty_emission.json');
-// for(js in json){
-//   var obj = new Emission();
-//   obj.item="electricity";
-//   obj.itemType="complex";
-//   obj.region=json[js]['Country'];
-//   obj.quantity=1;
-//   obj.units="kg/kWh";
-//   obj.categories=["electricity"];
-//   obj.components=[
-//     {
-//     	name: "generation",
-//     	quantity: 1,
-//     	units: "kg/kWh"
-//     },{
-//     	name: "td",
-//     	quantity: 1,
-//     	units: "kg/kWh"
-//     }]
-//     obj.save(function(err){
-//       if ( err ) throw err;
-//       console.log("Object Saved Successfully");
-//     });
-//     console.log(obj);
-// }
-// var obj = new Emission({
-//     item:"generation",
-//     itemType: "atomic",
-//     region: "Africa",
-//     quantity: 1,
-//     units: "kg/kWh",
-//     components: [{
-//     	name: "CO2",
-//     	quantity: 0.73576632,
-//     	units: "kgCO2/kWh"
-//     },{
-//     	name: "CH4",
-//     	quantity: 0.00001173634,
-//     	units: "kgCH4/kWh"
-//     },{
-//     	name: "N20",
-//     	quantity: 0.00000857091,
-//     	units: "kgCH4/kWh"
-//     }]
-// })
-// obj.save(function(err){
-//   if ( err ) throw err;
-//   console.log("Book Saved Successfully");
-// });
-Emission.collection().drop()
-// console.log(obj);
+for(js in json){
+  var obj = new Emission();
+  obj.item="electricity";
+  obj.itemType="complex";
+  obj.region=json[js]['Country'];
+  obj.quantity=1;
+  obj.units="kg/kWh";
+  obj.categories=["electricity"];
+  obj.components=[
+    {
+    	name: "generation",
+    	quantity: 1,
+    	units: "kg/kWh"
+    },{
+    	name: "td",
+    	quantity: 1,
+    	units: "kg/kWh"
+    }]
+  obj.save(function(err){
+    if ( err ) throw err;
+    console.log("Object Saved Successfully");
+  });
+  // console.log(obj);
+}
