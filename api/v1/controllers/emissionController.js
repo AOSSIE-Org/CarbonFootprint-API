0d7e8e8309863134b744b901ed5d7daa65cb5427
r0hit-gupta <rohit1997gupta@gmail.com>
2017-06-13 17:11:50 +0530

    added interpolate function

--- api/v1/controllers/emissionController.js
+++ api/v1/controllers/emissionController.js
@@ -1,5 +1,20 @@
 var Emission = require('../models/emissionModel');
 
+let interpolate = (l1, l2, d) => {
+    for(var x = 0; x < l1.length; x++){
+        if(d == l1[x]){
+            return l2[x];
+        }
+        if(d > l1[x] && d < l1[x+1] && x < l1.length - 1){
+            l1Floor = l1[x]
+            l1Ceil = l1[x+1];
+            l2Floor = l2[x]
+            l2Ceil = l2[x+1];
+            return l2Floor + ((l2Ceil - l2Floor)/(l1Ceil - l1Floor))*(d - l1Floor)
+        }
+    }
+}
+
 /*
  * A function to calculate the emissions of a component.
  * Refer to the Emission schema for more information on the components.
@@ -26,8 +41,8 @@ let find = (component, region, quantity) => {
                 if (item.components[0].name == 'CO2' ||
                     item.components[0].name == 'CH4' ||
                     item.components[0].name == 'N2O') {
-                    sum += (quantity * item.components[0].quantity);
-                    // console.log(`Emissions: ${sum} ${item.components[0].units}`);
+                    sum += (quantity * item.components[0].quantity[0]);
+                    console.log(`Emissions: ${sum} ${item.components[0].unit}`);
                     resolve(sum);
                 }
                 // if component type is complex, recurse to find its atomic components
@@ -35,13 +50,30 @@ let find = (component, region, quantity) => {
                     let numOfComponents = item.components.length; // number of subcomponents
                     (async function(){
                         for (let i = 0; i < numOfComponents; i++) {
-                        await find(item.components[i].name, region, item.components[i].quantity)
-                            .then((emis) => {
-                                sum += emis;
-                            })
-                            .catch((err) => console.log(err));
+                            if(item.components[i].quantity.length > 1){
+                                let calQuantity = await interpolate(item.quantity, item.components[i].quantity, quantity);
+                                console.log(`calQuantity = ${calQuantity}`)
+                                await find(item.components[i].name, region, calQuantity)
+                                        .then((emis) => {
+                                            sum += emis;
+                                        })
+                                        .catch((err) => console.log(err));
+                            }
+                            else {
+                                await find(item.components[i].name, region, item.components[i].quantity[0])
+                                        .then((emis) => {
+                                            sum += emis;
+                                        })
+                                        .catch((err) => console.log(err));
+                            }
                         }
-                    })().then(() => resolve(quantity * sum));
+                    })().then(() => {
+                        if(item.calculationMethod == 'interpolation'){
+                            resolve(sum);
+                        }
+                        else resolve(quantity * sum);
+                    })
+                    .catch((err) => console.log(err));
                 }
             } 
             // return an error if component is not found
@@ -52,7 +84,11 @@ let find = (component, region, quantity) => {
 
 exports.find = (req, res) => {
     console.log(req.body);
-    find(req.body["item"],req.body["region"],req.body["quantity"])
+    let itemName = req.body["item"];
+    let region = req.body["region"] || "Default";
+    let quantity = req.body["quantity"] || 1;
+
+    find(itemName, region, quantity)
         .then((sum) => {
             console.log(`\nTotal Emissions: ${sum}`);
             res.status(200).json({
