c2a13160163a0ef4ae0476d8901abe39953a4e87
Saisankar Gochhayat <saisankargochhayat@gmail.com>
2017-06-14 23:05:04 +0530

    added extrapolation function

--- api/v1/controllers/emissionController.js
+++ api/v1/controllers/emissionController.js
@@ -9,6 +9,10 @@ let interpolate = (l1, l2, d) => {
             l2Ceil = l2[x+1];
             return l2Floor + ((l2Ceil - l2Floor)/(l1Ceil - l1Floor))*(d - l1Floor)
         }
+        if(d >= l1[l1.length-1]){
+            let slope=Math.abs((l2[l2.length-1]-l2[l2.length-2])/(l1[l1.length-1]-l1[l1.length-2]));
+            return l2[l2.length-1]+(slope*(d-l1[l1.length-1]))
+        }
     }
 }
 
