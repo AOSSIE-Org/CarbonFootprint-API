8fd4a4c5222305c2e196febd4d41ddaa34d0c20d
r0hit-gupta <rohit1997gupta@gmail.com>
2017-06-25 01:19:51 +0530

    basic map added

--- client/src/components/Body/Body.js
+++ client/src/components/Body/Body.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import { Icon } from 'semantic-ui-react'
+import Choropleth from "../Choropleth/Choropleth";
+
 
 export default class Header extends React.Component {
     render() {
         return (
             <div style={styles.body}>
-                <div style={styles.card}></div>
+                <div style={styles.card}>
+                    <Choropleth />
+                </div>
             </div>
         );
     }
@@ -22,7 +26,10 @@ const styles = {
     card: {
         width: "100%",
         height: "100%",
-        background: '#ffffff',
+        background: '#fff',
+        backgroundImage: "url(data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAQAAAAECAYAAACp8Z5+AAAAGElEQVQYV2NkYGCQYmBgeMYABYwwBvECAD1WAQVZi1QaAAAAAElFTkSuQmCC)",
+        backgroundRepeat: "repeat",
+        backgroundSize: "7px",
         boxShadow: "1px 1px 5px #bbb",
     }
 }
--- client/src/components/Choropleth/Choropleth.js
+++ client/src/components/Choropleth/Choropleth.js
@@ -1,54 +1,57 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import 'topojson';
+import React, { Component } from "react"
+import { geoMercator, geoPath } from "d3-geo"
+import { feature } from "topojson-client"
 
-var topojson = require('topojson');
-var MapChoropleth = require('react-d3-map-choropleth').MapChoropleth;
+export default class WorldMap extends Component {
+    constructor() {
+        super()
+        this.state = {
+            worlddata: [],
+        }
 
-import './Choropleth.css';
-
-export default class Choropleth extends React.Component {
-
+        this.handleCountryClick = this.handleCountryClick.bind(this)
+    }
+    projection() {
+        return geoMercator()
+            .scale(100)
+            .translate([ 800 / 2, 450 / 2 ])
+    }
+    handleCountryClick(countryIndex) {
+        console.log("Clicked on country: ", this.state.worlddata[countryIndex])
+    }
+    componentDidMount() {
+        fetch("./world-110m.json")
+            .then(response => {
+                if (response.status !== 200) {
+                    console.log(`There was a problem: ${response.status}`)
+                    return
+                }
+                response.json().then(worlddata => {
+                    this.setState({
+                        worlddata: feature(worlddata, worlddata.objects.countries).features,
+                    })
+                })
+            })
+    }
     render() {
-        let width = 960;
-        let height = 600;
-
-        let topodata = require('json!../data/us.json');
-        let unemploy = require('dsv?delimiter=\t!../data/unemployment.tsv')
-
-        let dataStates = topojson.mesh(topodata, topodata.objects.states, function(a, b) { return a !== b; });
-        let dataCounties = topojson.feature(topodata, topodata.objects.counties).features;
-
-        let domain = {
-            scale: 'quantize',
-            domain: [0, .15],
-            range: d3.range(9).map(function(i) { return "q" + i + "-9"; })
-        };
-        let domainValue = function(d) { return +d.rate; };
-        let domainKey = function(d) {return +d.id};
-        let mapKey = function(d) {return +d.id};
-
-        let scale = 1280;
-        let translate = [width / 2, height / 2];
-        let projection = 'albersUsa';
-
-        return(
-            <MapChoropleth
-                width= {width}
-                height= {height}
-                dataPolygon= {dataCounties}
-                dataMesh= {dataStates}
-                scale= {scale}
-                domain= {domain}
-                domainData= {unemploy}
-                domainValue= {domainValue}
-                domainKey= {domainKey}
-                mapKey = {mapKey}
-                translate= {translate}
-                projection= {projection}
-                showGraticule= {true}
-            />
-        );
+        return (
+            <svg width="100%" height="100%" viewBox="0 0 800 450">
+                <g className="countries">
+                    {
+                        this.state.worlddata.map((d,i) => (
+                            <path
+                                key={ `path-${ i }` }
+                                d={ geoPath().projection(this.projection())(d) }
+                                className="country"
+                                fill={ `rgba(200,190,14,${1 / this.state.worlddata.length * i}` }
+                                stroke="#e8bb47"
+                                strokeWidth={ 0.5 }
+                                onClick={ () => this.handleCountryClick(i) }
+                            />
+                        ))
+                    }
+                </g>
+            </svg>
+        )
     }
 }
-
