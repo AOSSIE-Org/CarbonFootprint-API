3fba22bbfb7623f564458fb4e7c00ff979b48705
Saisankar Gochhayat <saisankargochhayat@gmail.com>
2017-07-04 21:50:39 +0530

    changed json loc

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var router = express.Router();
-var json = require('../public/data/percap.json');
+var json = require('../raw_data/percap.json');
 /* GET home page. */
 router.get('/', function(req, res, next) {
   var data={
