b0daa8a5f7b8c415c65702be42972184c261815b
Saisankar Gochhayat <saisankargochhayat@gmail.com>
2017-07-05 19:44:38 +0530

    bar graph working for afghanistan

--- api/v1/frontend-scripts/percap.js
+++ api/v1/frontend-scripts/percap.js
@@ -2,14 +2,12 @@ var percap = require('../../../raw_data/percap.json');
 var obj={};
 for (js in percap){
   obj[percap[js]["Country"]]={
-      "years":[],
-      "values":[]      
+      
   }
-for (i = 0; i < 28; i++) {
-    obj[percap[js]["Country"]].years[i]=1990+i
-    obj[percap[js]["Country"]].values[i]=percap[js][obj[percap[js]["Country"]].years[i]]
+for (i = 0; i < 22; i++) {
+    currYear=1990+i;
+    obj[percap[js]["Country"]][currYear]=percap[js][currYear];
 }
 }
-
 jsonStr = JSON.stringify(obj);
 console.log(jsonStr);
