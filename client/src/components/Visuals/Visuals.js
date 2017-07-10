70bebc58c1362209e665776d07b0742385f8c2d8
r0hit-gupta <rohit1997gupta@gmail.com>
2017-07-10 23:18:47 +0530

    changed description of cards

--- client/src/components/Visuals/Visuals.js
+++ client/src/components/Visuals/Visuals.js
@@ -23,14 +23,14 @@ export default class Visuals extends React.Component {
   <Grid.Column>
   <Link to="/percapita" ><Card>
     <Card.Content header='Per Capita &#x1F464;' />
-    <Card.Content description="Get to see all the emissions due to electricity generation all over the world." />
+    <Card.Content description="See the average CO2 emitted by each person in various countries over the past years." />
 
   </Card></Link>
 </Grid.Column>
 <Grid.Column>
   <Link to="/flights" ><Card>
     <Card.Content header='Airplanes &#x1F6E9;' />
-    <Card.Content description="Get to see all the emissions due to electricity generation all over the world." />
+    <Card.Content description="Get to see the emissions of an airplane model of all kinds." />
 
   </Card></Link>
 </Grid.Column>
