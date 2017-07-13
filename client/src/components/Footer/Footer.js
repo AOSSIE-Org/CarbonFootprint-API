a2f5d2c0360cee8c44337903c58624620f9282b9
r0hit-gupta <rohit1997gupta@gmail.com>
2017-07-13 23:39:54 +0530

    added footer component

---
+++ client/src/components/Footer/Footer.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const Footer = () => (
+            <div style={styles.footer}>
+                <strong>Carbon Footprint</strong>
+            </div>
+        );
+
+const styles = {
+    footer: {
+        width: "100%",
+        height: "80px",
+        marginTop: "50px",
+        background: "#ddd",
+        padding: "10px 30px"
+    }
+}
+
+export default Footer;
+
