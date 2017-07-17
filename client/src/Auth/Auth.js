118e61483c2fe51a539bb44e190a31107a053fe5
r0hit-gupta <rohit1997gupta@gmail.com>
2017-07-17 12:49:00 +0530

    auth added

--- client/src/Auth/Auth.js
+++ client/src/Auth/Auth.js
@@ -3,20 +3,19 @@ import auth0 from 'auth0-js';
 import { AUTH_CONFIG } from './auth0-config';
 
 export default class Auth {
-  auth0 = new auth0.WebAuth({
-    domain: AUTH_CONFIG.domain,
-    clientID: AUTH_CONFIG.clientId,
-    redirectUri: AUTH_CONFIG.callbackUrl,
-    audience: `https://${AUTH_CONFIG.domain}/userinfo`,
-    responseType: 'token id_token',
-    scope: 'openid'
-  });
-
   constructor() {
     this.login = this.login.bind(this);
     this.logout = this.logout.bind(this);
     this.handleAuthentication = this.handleAuthentication.bind(this);
     this.isAuthenticated = this.isAuthenticated.bind(this);
+    this.auth0 = new auth0.WebAuth({
+      domain: AUTH_CONFIG.domain,
+      clientID: AUTH_CONFIG.clientId,
+      redirectUri: AUTH_CONFIG.callbackUrl,
+      audience: `https://${AUTH_CONFIG.domain}/userinfo`,
+      responseType: 'token id_token',
+      scope: 'openid'
+    });
   }
 
   login() {
--- client/src/Auth/auth0-config.js
+++
@@ -1,5 +0,0 @@
-export const AUTH_CONFIG = {
- domain: 'carbonfootprint.auth0.com',
- clientId: 'tEOKbbCEUE3KuSoipVBpMQ3pRrOpXZms',
- callbackUrl: 'http://localhost:3080/callback'
-}
