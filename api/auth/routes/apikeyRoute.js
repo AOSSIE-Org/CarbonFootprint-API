79fb07f1cdfec087a509bd346f7fd7468229f12c
Saisankar Gochhayat <saisankargochhayat@gmail.com>
2017-07-22 23:42:15 +0530

    routes added

--- api/auth/routes/apikeyRoute.js
+++ api/auth/routes/apikeyRoute.js
@@ -3,14 +3,31 @@ var router = express.Router();
 // get the auth controller
 const Auth = require('../controllers/authController');
 
-router.post('/', (req, res) => {
+router.post('/key', (req, res) => {
     let email = req.user.email;
-    let action = req.body.action;
-    Auth.auth(email,action);
+    let action = "create";
+    Auth.auth(email, action);
     res.status(200).json({
-                success: true,
-                action: req.body.action
-            });
+        success: true,
+        action: req.body.action
+    });
 });
-
+router.get('/key', (req, res) => {
+    let email = req.user.email;
+    let action = "retrieve";
+    Auth.auth(email, action);
+    res.status(200).json({
+        success: true,
+        action: req.body.action
+    });
+});
+router.delete('/key', (req, res) => {
+    let email = req.user.email;
+    let action = "revoke";
+    Auth.auth(email, action);
+    res.status(200).json({
+        success: true,
+        action: req.body.action
+    });
+});
 module.exports = router;
