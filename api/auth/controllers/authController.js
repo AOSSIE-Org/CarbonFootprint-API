059f73d4893fd5486dc912bce50ee97256e2dee0
Saisankar Gochhayat <saisankargochhayat@gmail.com>
2017-07-22 16:58:42 +0530

    Auth Controller functions defined

--- api/auth/controllers/authController.js
+++ api/auth/controllers/authController.js
@@ -6,33 +6,80 @@ const uuidv5 = require('uuid/v5');
 // Generate a api key using random and the provided email.
 let generateApiKey = (email) => {
     const random = uuidv4(); // 
-    return(uuidv5(email, random));
+    return (uuidv5(email, random));
 }
 // Create and store apikey
 let createApiKey = (email) => {
     let apiKey = generateApiKey(email);
     var user = new User();
-    user.email = "email";
-    user.apiKey = apiKey;
+    user.email = email;
+    user.apikey = apiKey;
     user.ratelimit = 1000;
-    user.save(function(err){
-    if ( err ) throw err;
-    console.log("User API Key Saved");
+    user.save(function (err) {
+        if (err) throw err;
+        console.log("User API Key Saved");
+    });
+}
+//Retreive API Key
+let retreiveApiKey = (email) => {
+    return new Promise((resolve, reject) => {
+        // find the user in the database
+        User.findOne({
+            $or: [{
+                'email': new RegExp(`^${email}$`, "i"),
+            }]
+        }, (err, user) => {
+            // if user is found
+            if (!err && user) {
+                resolve(user.apikey);
+            }
+            else reject(`Unable to find user`);
+        });
+    });
+}
+//Revoke API Key
+let revokeApiKey = (email) => {
+    return new Promise((resolve, reject) => {
+        // find the user in the database
+        User.findOne({
+            $or: [{
+                'email': new RegExp(`^${email}$`, "i"),
+            }]
+        }, (err, user) => {
+            // if user is found
+            if (!err && user) {
+                user.remove();
+                resolve("removed user");
+            }
+            else reject(`Unable to find user`);
+        });
     });
 }
 
 //Function to create , store , revoke apikey.
 //Supported actions - create , retreive , revoke
 let apiKey = (mail, action) => {
-    if (action == "create"){
+    if (action == "create") {
         createApiKey(mail);
-        return("success");
+        return true;
     }
-    if (action == "retreive"){
-
+    if (action == "retreive") {
+        let apiToken = retreiveApiKey(mail);
+        apiToken.then(function (result) {
+            console.log(result) 
+        })
+        return true;
     }
-    if (action == "revoke"){
-
-    }    
+    if (action == "revoke") {
+        let apiToken = revokeApiKey(mail);
+        apiToken.then(function (result) {
+            console.log(result) 
+        })
+        return true;
+    }
 }
-console.log(apiKey("saisankargochhayat@gmail.com","create"));
+
+exports.auth = async function (email, action) {
+    let auth = await apiKey(email, action);
+    return true;
+}
--- api/auth/routes/apikeyRoute.js
+++ api/auth/routes/apikeyRoute.js
@@ -1,8 +1,12 @@
 var express = require('express');
 var router = express.Router();
 // get the auth controller
-const Auth = require('../controllers/emissionController');
+const Auth = require('../controllers/authController');
+
 router.post('/', (req, res) => {
+    let email = req.user.email;
+    let action = req.body.action;
+    Auth.auth(email,action);
     res.status(200).json({
                 success: true,
                 action: req.body.action
