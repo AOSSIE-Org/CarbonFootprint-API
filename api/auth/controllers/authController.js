f37286dafa012ca1a01b4060286d6c61496b2723
Saisankar Gochhayat <saisankargochhayat@gmail.com>
2017-07-24 00:00:31 +0530

    working retreive and delete

--- api/auth/controllers/authController.js
+++ api/auth/controllers/authController.js
@@ -15,7 +15,8 @@ const retrieveApiKey = email => {
   return new Promise((resolve, reject) => {
     // find the user in the database
     User.findOne({ email: email }, (err, user) => {
-      if (!err && user) resolve(user);
+      if (!err && user) {
+        resolve(user.apikey)}
       else reject('Unable to find the user');
     });
   });
@@ -57,37 +58,42 @@ const deleteApiKey = email => {
 //Function to create , store , revoke apikey.
 //Supported actions - create , retreive , revoke
 let apiKey = (mail, action) => {
+  return new Promise((resolve, reject) => {
   if (action == 'create') {
-    createApiKey(mail);
-    return true;
+    let create = createApiKey(mail)
+    create.then(function(result) {
+        resolve(result)
+      })
+      .catch(function(reject) {
+        reject("User not found")
+      });
   }
   if (action == 'retrieve') {
     let apiToken = retrieveApiKey(mail);
     apiToken
       .then(function(result) {
-        console.log(result);
+        resolve(result)
       })
-      .catch(function(reject) {
-        console.log('user not found');
+      .catch(function(err) {
+        reject(err)
       });
-    return true;
   }
   if (action == 'revoke') {
     let apiToken = deleteApiKey(mail);
     apiToken
       .then(function(result) {
-        console.log(result);
+        resolve(result)
       })
-      .catch(function(reject) {
-        console.log('user not found');
+      .catch(function(err) {
+        reject(err)
       });
-    return true;
   }
+  });
 };
 
 exports.auth = async function(email, action) {
   let auth = await apiKey(email, action);
-  return true;
+  return auth;
 };
 
 //Verify API Key
@@ -114,7 +120,7 @@ let verifyApiKey = (apikey) => {
                    resolve(user.ratelimit); 
                 }
                 else{
-                  console.log("Your API call limits are deplenished");
+                  reject("Your API call limits are deplenished");
                 }
             } else reject(`Unable to find user`);
         });
@@ -124,5 +130,4 @@ let verifyApiKey = (apikey) => {
 //Checkts if the API exists and decreases the ratelimit by 1 and executes the API call.
 exports.verifyapikey = async function (req, res) {
     let verifykey = await verifyApiKey(req);
-    return verifykey;
 }
--- api/auth/routes/apikeyRoute.js
+++ api/auth/routes/apikeyRoute.js
@@ -6,28 +6,56 @@ const Auth = require('../controllers/authController');
 router.post('/key', (req, res) => {
     let email = req.user.email;
     let action = "create";
-    Auth.auth(email, action);
-    res.status(200).json({
-        success: true,
-        action: req.body.action
-    });
+    let create = Auth.auth(email,action)
+    create.then(function (result) {
+            console.log(result);
+            res.status(200).json({
+                success: true,
+                create: result
+            });
+        })
+        .catch(function (reject) {
+            res.status(200).json({
+                success: true,
+                err: reject
+            });
+        });
 });
+
 router.get('/key', (req, res) => {
     let email = req.user.email;
     let action = "retrieve";
-    Auth.auth(email, action);
-    res.status(200).json({
-        success: true,
-        action: req.body.action
-    });
+    let retrieve = Auth.auth(email, action)
+    retrieve.then(function (result) {
+            console.log(result);
+            res.status(200).json({
+                success: true,
+                apikey: result
+            });
+        })
+        .catch(function (reject) {
+            res.status(200).json({
+                success: true,
+                err: reject
+            });
+        });
 });
+
 router.delete('/key', (req, res) => {
     let email = req.user.email;
     let action = "revoke";
-    Auth.auth(email, action);
-    res.status(200).json({
-        success: true,
-        action: req.body.action
-    });
+    Auth.auth(email, action).then(function (result) {
+            res.status(200).json({
+                success: true,
+                deleted: req.user.email
+            });
+        })
+        .catch(function (reject) {
+            res.status(200).json({
+                success: true,
+                err: "User not found"
+            });
+        });
 });
+
 module.exports = router;
