e475e0ac02a5510cafa6240ef10c82ea12fa34bd
Saisankar Gochhayat <saisankargochhayat@gmail.com>
2017-07-28 17:03:53 +0530

    time stamp added to schema

--- api/auth/models/userModel.js
+++ api/auth/models/userModel.js
@@ -3,19 +3,21 @@ var Schema = mongoose.Schema;
 
 // create a schema for for generation of user api keys and rate limiting.
 var userSchema = new Schema({
-    email: {
-    	type: String,
-    	required: true
-    },
-    apikey: {
-    	type: String,
-    	required: true,
-    },
-    ratelimit: {
-    	type: Number,
-    	required: true,
-    	default: 1000
-    }
+	email: {
+		type: String,
+		required: true
+	},
+	apikey: {
+		type: String,
+		required: true,
+	},
+	ratelimit: {
+		type: Number,
+		required: true,
+		default: 1000
+	}
+}, {
+	timestamps: true
 })
 
-module.exports =  mongoose.model('User', userSchema);
+module.exports = mongoose.model('User', userSchema);
