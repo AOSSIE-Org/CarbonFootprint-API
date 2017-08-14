4cf4e0297a9e1e05efe9de64559455c146d7f6fe
Saisankar Gochhayat <saisankargochhayat@gmail.com>
2017-08-14 18:30:07 +0530

    minor fix

--- api/v1/db_scripts/appliances_db.js
+++ api/v1/db_scripts/appliances_db.js
@@ -3,11 +3,10 @@
 var mongoose = require('mongoose');
 // get the database configuration file
 try {
-	var config = require('../../../config.json');
-}
-catch(e){
-	console.log(`Database configuration file "config.json" is missing.`);
-	process.exit(1);
+  var config = require('../../../config.json');
+} catch (e) {
+  console.log(`Database configuration file "config.json" is missing.`);
+  process.exit(1);
 }
 var db = config.database;
 
@@ -15,37 +14,36 @@ var db = config.database;
 mongoose.connect(`mongodb://${db.username}:${db.password}@${db.hostname}:${db.port}/${db.dbname}`);
 
 // When successfully connected
-mongoose.connection.on('connected', () => {  
-    console.log('Connection to database established successfully');
-    console.log("electricity_db.js running");
-}); 
+mongoose.connection.on('connected', () => {
+  console.log('Connection to database established successfully');
+  console.log("electricity_db.js running");
+});
 
 // If the connection throws an error
-mongoose.connection.on('error', (err) => {  
+mongoose.connection.on('error', (err) => {
   console.log('Error connecting to database: ' + err);
-}); 
+});
 
 // When the connection is disconnected
-mongoose.connection.on('disconnected', () => {  
-  console.log('Database disconnected'); 
+mongoose.connection.on('disconnected', () => {
+  console.log('Database disconnected');
 });
 var json = require('../../../raw_data/appliances.json');
-for(js in json){
-var Emission = require('../models/emissionModel.js')
+for (js in json) {
+  var Emission = require('../models/emissionModel.js')
   var obj = new Emission();
-  obj.item=json[js]['Appliance'];
-  obj.region="Default";
-  obj.quantity=[1];
-  obj.unit="kWh";
-  obj.categories=["appliances"];
-  obj.components=[
-    {
-    	name: "electricity",
-    	quantity: json[js]['Average_watts (in Wh)']/1000,
-    	unit: "kWh"
-    }]
-  obj.save(function(err){
-    if ( err ) throw err;
+  obj.item = json[js]['Appliance'];
+  obj.region = "Default";
+  obj.quantity = [1];
+  obj.unit = "kWh";
+  obj.categories = ["appliances"];
+  obj.components = [{
+    name: "electricity",
+    quantity: json[js]['Average_watts (in Wh)'] / 1000,
+    unit: "kWh"
+  }]
+  obj.save(function (err) {
+    if (err) throw err;
     console.log("Object Saved Successfully");
   });
   // console.log(obj);
