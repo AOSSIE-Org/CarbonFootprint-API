a9ddc301291b78f40bf1ed8260d38f44ea6fafce
Subhang23 <vsubhang4@gmail.com>
2020-05-30 17:53:59 +0530

    changed url name for UI config

--- client/src/config/config.js
+++ client/src/config/config.js
@@ -1,2 +1,2 @@
-export const API_URL = (process.env.NODE_ENV === 'production') ? 'https://carbonhub.org' : 'http://localhost:3000';
+export const UI_URL = (process.env.NODE_ENV === 'production') ? 'https://carbonhub.org' : 'http://localhost:3000';
 export const API_URL_SERVER = (process.env.NODE_ENV === 'production') ? 'https://carbonhub.org' : 'http://localhost:3080';
